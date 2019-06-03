331fef3eeeecda2650775eefa77d70d0bca0695d
Suraj25P <surajrprabhu99@gmail.com>
2019-06-03 20:52:54 +0530

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -31,20 +31,119 @@ function nyfunc() {
     }
   }
 
-
-fetch("/postQuestion", {
+  fetch("/postQuestion", {
     method: "POST",
-    body :JSON.stringify(names),
-    headers: {     
+    body: JSON.stringify(names),
+    headers: {
       "Content-Type": " application/json"
     }
-  }
-  )
+  })
     .then(res => res.json())
     .then(data => {
-      document.getElementById('msgholder').innerHTML = data.msg;
+      document.getElementById("msgholder").innerHTML = data.msg;
     })
     .catch(err => {
       console.log(err);
-})
+    });
+}
+
+//******************************************** */USER CLIENT-SIDE JAVASCRIPT
+function attemptChecker() {
+  let qid = document.getElementById("quizID").value;
+  fetch("/getQuestion", {
+    method: "POST",
+    body: qid,
+    headers: {
+      "Content-Type": " text/plain"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("msgHolder1").innerHTML = data.msg;
+    })
+
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function takeTest() {
+  let qid = document.getElementById("quizID").value;
+  fetch("/takeTest", {
+    method: "POST",
+    body: JSON.stringify({ qid: qid }),
+    headers: {
+      "Content-Type": " application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      for (i = 0; i < data.length; i++) {
+        let output = `  <div>
+                <br/>
+                <br/>
+                <input id= type="text"  class="question" value='${data[i].question}'/>
+                <br/>
+                <label for="a">Option A</label>
+                <input id="a" type="text"  class="question" value='${data[i].opa}'/>
+                <br/>
+                <label for="b">Option B</label>
+                <input id="b" type="text"  class="question" value='${data[i].opb}' />
+                <br/>
+                <label for="c">Option C</label>                
+                <input id="c" type="text"  class="question" value='${data[i].opc}'/>
+                <br/>
+                <label for="d">Option D</label>
+                <input id="d" type="text"  class="question" value='${data[i].opd}'/>
+                <br/>
+                <input type="text"  placeholder="Enter correct answer" class="answer" />
+                <br/>
+                <label for="m">Marks:</label>
+                <input id="m" type="number"  class="question" value='${data[i].mpq}' />
+                <br/>
+                <br/>
+                <hr/>
+                </div>`;
+        let node = document.createElement("div");
+        node.innerHTML = output;
+        document.getElementById("questionPaperHolder").appendChild(node);
+      }
+       let button=`<button onClick="submitTest()">Submit</button>`
+       let node = document.createElement("div");
+        node.innerHTML = button;
+        document.getElementById("questionPaperHolder").appendChild(node);
+    })
+
+    .catch(err => {
+      console.log("error1j");
+      console.log(err);
+    });
+}
+
+
+//****************************************Score calculation ********************************/
+function submitTest(){
+  var elem = document.getElementsByClassName("answer");
+  var answers = [];
+  for (var i = 0; i < elem.length; ++i) {
+    if (typeof elem[i].value !== "undefined") {
+      answers.push(elem[i].value);
+    }
+  }
+
+  fetch("/submitTest", {
+    method: "POST",
+    body: JSON.stringify(answers),
+    headers: {
+      "Content-Type": " application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("msgholder").innerHTML = data.msg;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
 }
--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
 const mysql = require("mysql");
 var db = mysql.createConnection({
   host: "localhost",
@@ -22,11 +22,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.text());
+app.use(cookieParser());
 
 app.use("/static", express.static(path.join(__dirname, "../", "public")));
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("listening to 3000");
 });
 
@@ -39,7 +40,7 @@ app.get("/", (req, res) => {
 app.post("/next", (req, res) => {
   const { uname, psw } = req.body;
   let sql = `select * from admins where un= '${uname}' and pw= '${psw}'`;
-  let sql1 = `select * from asers where un= '${uname}' and pw= '${psw}'`;
+  let sql1 = `select * from users where un= '${uname}' and pw= '${psw}'`;
   if (req.body.grp == "Admin") {
     db.query(sql, (err, result) => {
       if (err) {
@@ -53,9 +54,12 @@ app.post("/next", (req, res) => {
       }
     });
   } else {
+    res.cookie("sid", uname);
+
     db.query(sql1, (err, result) => {
       if (err) {
         res.send("error");
+        console.log(err);
       } else if (result.length == 0) {
         res.send("incorrect username or password");
       } else {
@@ -67,12 +71,11 @@ app.post("/next", (req, res) => {
   }
 });
 
-
 app.post("/postQuestion", (req, res) => {
   let sql3 = `select * from ${req.body[0]}`;
-  quizdb.query(sql3,(err,result)=>{
-    if(err){ 
-      let sql4 =`CREATE TABLE ${req.body[0]} (
+  quizdb.query(sql3, (err, result) => {
+    if (err) {
+      let sql4 = `CREATE TABLE ${req.body[0]} (
         question varchar(20),
         opa varchar(20),
         opb varchar(20),
@@ -81,31 +84,88 @@ app.post("/postQuestion", (req, res) => {
         cans varchar(20),
         mpq int
       );`;
-      quizdb.query(sql4,(err,result)=>{
-        if(err){
-          res.json({ msg: "coudnt create Quiz...try again" })
-          
-        }
-        else{
-          var n=req.body[1];
-          var o=0;
-          for(i=0;i<n;i++)
-          {
-            let sql5=`INSERT INTO ${req.body[0]} VALUES ( '${req.body[2+o]}' , '${req.body[3+o]}','${req.body[4+o]}', '${req.body[5+o]}','${req.body[6+o]}','${req.body[7+o]}',${req.body[8+o]});`;
-            quizdb.query(sql5,(err,result)=>{
-              if(err){
-                res.json({ msg: "coudnt upload data!" })
-              }             
-            })
-           o+=7
+      quizdb.query(sql4, (err, result) => {
+        if (err) {
+          res.json({ msg: "coudnt create Quiz...try again" });
+        } else {
+          var n = req.body[1];
+          var o = 0;
+          for (i = 0; i < n; i++) {
+            let sql5 = `INSERT INTO ${req.body[0]} VALUES ( '${
+              req.body[2 + o]
+              }' , '${req.body[3 + o]}','${req.body[4 + o]}', '${
+              req.body[5 + o]
+              }','${req.body[6 + o]}','${req.body[7 + o]}',${req.body[8 + o]});`;
+            quizdb.query(sql5, (err, result) => {
+              if (err) {
+                res.json({ msg: "coudnt upload data!" });
+              }
+            });
+            o += 7;
           }
-            res.json({ msg: "Successfully created..!!" }) 
+          res.json({ msg: "Successfully created..!!" });
         }
-      })   
+      });
+    } else {
+      res.json({ msg: "this Quiz id already exists" });
     }
-    else{
-      res.json({ msg: "this Quiz id already exists" })
+  });
+});
+
+app.post("/getQuestion", (req, res) => {
+
+  let sql = `select * from scoreboard where qid='${req.body}' and sid='${
+    req.cookies.sid
+    }';`;
+  db.query(sql, (err, result) => {
+    if (result.length != 0) {
+      res.json({
+        msg: `you have already attempted this test and your score is ${
+          result[0].score
+          }`
+      });
+    }
+  });
+});
+app.post("/takeTest", (req, res) => {
+  var x = req.body.qid
+  res.cookie('qid', x);
+  let sql = `select * from ${req.body.qid}`;
+  quizdb.query(sql, (err, result) => {
+    if (err) {
+      res.json({ msg: "This quiz doesnt exist yet..!" });
+    } else {
+      var x = { ...result }
+      res.json(result);
+
 
     }
   });
+});
+
+app.post('/submitTest', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies.qid);
+  console.log(req.cookies.sid);
+  let sql = `select cans,mpq from ${req.cookies.qid}`;
+  quizdb.query(sql, (err, result) => {
+    if (result) {
+      var score = 0;
+      console.log(req.body.length);
+      for (i = 0; i < req.body.length; i++) {
+        if (req.body[i] == result[i].cans) {
+          score = score + result[i].mpq;
+        }
+      }
+      let sql1= `insert into scoreboard values(${req.cookies.qid} ,${req.cookies.sid},${score});`;
+      db.query(sql1,(err,result)=>{
+        if(err){
+          res.json({msg:'something went worng..!!'})
+        }else{
+          res.json({msg:`your answer has been submitted and your score is ${score}`});
+        }
+      })
+    }
+  })
+
 });
